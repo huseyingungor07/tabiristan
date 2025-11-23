777e4a7be94abbf9322cb42cdf67d6def89d43d4
Hüseyin Güngör <07huseyingungor@gmail.com>
2025-11-24 02:14:19 +0300

    [200~Analytics modülü onarildi ve tsconfig guncellendi~

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import GoogleAnalytics from '../components/GA'; 
+import Analytics from '../components/Analytics';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {/* G- Kodunu buraya yazıyorsun nnn*/}
-        <GoogleAnalytics gaId="G-8EKSSYR1WK" />
+        <Analytics gaId="G-8EKSSYR1WK" />
 
         {children}
       </body>
