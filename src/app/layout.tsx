bbac527556e33114fb87ac8cde607e57217fc1e3
Hüseyin Güngör <07huseyingungor@gmail.com>
2025-11-24 02:02:54 +0300

    gogleanalitikkkk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* G- Kodunu buraya yazıyorsun */}
+        {/* G- Kodunu buraya yazıyorsun nnn*/}
         <GoogleAnalytics gaId="G-8EKSSYR1WK" />
 
         {children}
