ef3426faff24e4415cbcc9d3337a44840ce61de5
Hüseyin Güngör <07huseyingungor@gmail.com>
2025-11-24 02:22:45 +0300

    slgslgklk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Analytics from '../components/Analytics';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {/* G- Kodunu buraya yazıyorsun nnn*/}
-        <Analytics gaId="G-8EKSSYR1WK" />
+        <GoogleAnalytics gaId="G-8EKSSYR1WK" />
 
         {children}
       </body>
