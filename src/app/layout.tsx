eaf4cd54b8f1bec5b4152c1ff3f8a52f25940bcb
Hüseyin Güngör <07huseyingungor@gmail.com>
2025-11-24 01:56:57 +0300

    google analytics eklendi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import GoogleAnalytics from '@/components/GoogleAnalytics'; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,8 +26,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
+      
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* G- Kodunu buraya yazıyorsun */}
+        <GoogleAnalytics gaId="G-8EKSSYR1WK" />
+
         {children}
       </body>
     </html>
